306d662b0804438939932b2272e87f2a34ea3bcc
themostdavid <38319257+themostdavid@users.noreply.github.com>
2018-06-25 08:54:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,15 +4,7 @@ function startGame()
 {
 	gameArea.start();
 	
-	gamePiece = new component(20, 20, "#5500AA", 12, 12);
-}
-
-function updatePlayerInfo()
-{
-	document.getElementById("xPos").innerHTML = "X-position = " + gamePiece.x;
-	document.getElementById("yPos").innerHTML = "Y-position = " + gamePiece.y;
-	document.getElementById("xVel").innerHTML = "X-velocity = " + gamePiece.speedX;
-	document.getElementById("yVel").innerHTML = "Y-velocity = " + gamePiece.speedY;
+	gamePiece = new player(20, 20, "#5500AA", gameArea.canvas.width/2, 530);
 }
 
 function updatePieceColor()
@@ -42,25 +34,11 @@ function updatePieceColor()
 		green = 0;
 		blue = 255 - (xPos - 513);
 	}
-	gamePiece.color = "rgba("+red+","+green+","+blue+","+alpha+")";
+	gamePiece.color = "rgba("+red+","+green+","+blue+",1)";
 }
 
-function updateGameArea()
-{
-	//gameArea.clear();
-	gamePiece.deccelerate();
-	if(gameArea.keys && gameArea.keys[37]) {gamePiece.speedX -= 1;}
-	if(gameArea.keys && gameArea.keys[39]) {gamePiece.speedX += 1;}
-	if(gameArea.keys && gameArea.keys[38]) {gamePiece.speedY -= 1;}
-	if(gameArea.keys && gameArea.keys[40]) {gamePiece.speedY += 1;}
-	gamePiece.newPos();
-	updatePieceColor();
-	gamePiece.update();
-	updatePlayerInfo();
-	
-}	
-
-function component(width, height, color, x, y)
+//Handles creation of "player" objects
+function player(width, height, color, x, y)
 {
 	this.width = width;
     this.height = height;
@@ -69,7 +47,7 @@ function component(width, height, color, x, y)
     this.y = y;
 	this.speedX = 0;
 	this.speedY = 0;
-	this.friction = .25;
+	this.friction = 1;
 	this.update = function()
 	{
 		ctx = gameArea.context;
@@ -78,10 +56,10 @@ function component(width, height, color, x, y)
 	};
 	this.newPos = function()
 	{
-		var xBorder = gameArea.canvas.width-21;
-		var yBorder = gameArea.canvas.height-21;
+		var xBorder = gameArea.canvas.width-36;
+		var yBorder = gameArea.canvas.height-36;
 		
-		if((this.x <= xBorder) && (this.x >= 1))
+		if((this.x <= xBorder) && (this.x >= 16))
 		{
 			this.x += this.speedX;
 		}
@@ -92,10 +70,10 @@ function component(width, height, color, x, y)
 				this.speedX = 0;
 				this.x = xBorder;
 			}
-			else if(this.x < 1)
+			else if(this.x < 16)
 			{
 				this.speedX = 0;
-				this.x = 1;
+				this.x = 16;
 			}
 		}
 		if((this.y <= yBorder) && (this.y >= 1))
@@ -154,29 +132,58 @@ function component(width, height, color, x, y)
 	}
 }
 
+function updateGameArea()
+{
+	gameArea.clear();
+	gamePiece.deccelerate();
+	if(gameArea.keys && gameArea.keys[37] && (gamePiece.x > 16)) {gamePiece.speedX = -5;}
+	if(gameArea.keys && gameArea.keys[39] && (gamePiece.x < (gameArea.canvas.width-36))) {gamePiece.speedX = 5;}
+	//if(gameArea.keys && gameArea.keys[38]) {gamePiece.speedY = -5;}
+	//if(gameArea.keys && gameArea.keys[40]) {gamePiece.speedY = 5;}
+	gamePiece.update();
+	gamePiece.newPos();
+	//updatePieceColor();
+	
+	updatePlayerInfo();
+}
+
+function updatePlayerInfo()
+{
+	document.getElementById("xPos").innerHTML = "X-position = " + gamePiece.x;
+	document.getElementById("yPos").innerHTML = "Y-position = " + gamePiece.y;
+	document.getElementById("xVel").innerHTML = "X-velocity = " + gamePiece.speedX;
+	document.getElementById("yVel").innerHTML = "Y-velocity = " + gamePiece.speedY;
+}
+
 var gameArea =
 {
 	canvas : document.createElement("canvas"),
     start : function() 
 	{
-        this.canvas.width = 640;
-        this.canvas.height = 360;
+        this.canvas.width = 380;
+        this.canvas.height = 580;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-		this.internval = setInterval(updateGameArea, 20);
-		window.addEventListener("keydown", function(e)
+		this.internval = setInterval(updateGameArea, 50/3);
+		
+		$(function() 
 		{
-			gameArea.keys = (gameArea.keys || []);
-            gameArea.keys[e.keyCode] = true;
-		})
-		window.addEventListener("keyup", function(e)
-		{
-			gameArea.keys[e.keyCode] = false;
-		})
+		   $(window).keydown(function(e) 
+		   {
+				var key = e.which;
+				console.log(key);
+				gameArea.keys = (gameArea.keys || []);
+				gameArea.keys[key] = true;
+		   });
+		   $(window).keyup(function(e)
+		   {
+			   gameArea.keys[e.which] = false;
+		   });
+		});
+
 	},
 	clear : function()
 	{
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	}
 }
-
